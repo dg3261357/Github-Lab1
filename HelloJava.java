f99d42c4b882fba64054601b97f8314f898cbea0
root <dg3261357@gmail.com>
2021-03-14 23:58:35 +0000

    I am pushing all the files to my GitHub

---
+++ HelloJava.java
@@ -0,0 +1,5 @@
+class HelloJava {
+public static void main(String args[]){
+System.out.Println("A Java program for Github-lab1. Thank you!");
+	}	
+}
---
+++ index.js
@@ -0,0 +1,2 @@
+var texts="I am your JavaScript-Github-Lab1 Program";
+console.log(texts);
